6a0ebbcc467eea00f4c0be67b9a6a9f41468392f
vincenzosomma <77669617+vincenzosomma@users.noreply.github.com>
2021-06-13 18:50:29 +0100

    register users into the DB

--- app/src/main/java/com/viso/cybertreev1/activities/SignUpActivity.kt
+++ app/src/main/java/com/viso/cybertreev1/activities/SignUpActivity.kt
@@ -3,11 +3,15 @@ package com.viso.cybertreev1.activities
 import android.os.Bundle
 import android.text.TextUtils
 import android.view.View
+import android.view.WindowManager
 import android.widget.Switch
 import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.viso.cybertreev1.R
 import kotlinx.android.synthetic.main.activity_sign_up.*
-
 
 class SignUpActivity : BaseActivity() {
 
@@ -19,6 +23,11 @@ class SignUpActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         // This is used to align the xml view to this class
         setContentView(R.layout.activity_sign_up)
+        // This is used to hide the status bar and make the splash screen as a full screen activity.
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
 
         setupActionBar()
 
@@ -65,20 +74,63 @@ class SignUpActivity : BaseActivity() {
         val confirmPassword: String = et_password_confirm.text.toString().trim { it <= ' ' }
 
 
-
+        if (password == confirmPassword) {
         if (validateForm(name, email, password, confirmPassword)) {
-                if (password == confirmPassword) {
-                Toast.makeText(
-                    this@SignUpActivity,
-                    "Now we can register a new user.",
-                    Toast.LENGTH_SHORT
-                ).show()
+            // Show the progress dialog.
+            showProgressDialog(resources.getString(R.string.please_wait))
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(
+                    OnCompleteListener<AuthResult> { task ->
+
+                        // Hide the progress dialog
+                        hideProgressDialog()
+
+                        // If the registration is successfully done
+                        if (task.isSuccessful) {
+
+                            // Firebase registered user
+                            val firebaseUser: FirebaseUser = task.result!!.user!!
+                            // Registered Email
+                            val registeredEmail = firebaseUser.email!!
+
+                            Toast.makeText(
+                                this@SignUpActivity,
+                                "$name you have successfully registered with email id $registeredEmail.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+
+                            /**
+                             * Here the new user registered is automatically signed-in so we just sign-out the user from firebase
+                             * and send him to Intro Screen for Sign-In
+                             */
+
+                            /**
+                             * Here the new user registered is automatically signed-in so we just sign-out the user from firebase
+                             * and send him to Intro Screen for Sign-In
+                             */
+                            FirebaseAuth.getInstance().signOut()
+                            // Finish the Sign-Up Screen
+                            finish()
+                        } else {
+                            Toast.makeText(
+                                this@SignUpActivity,
+                                task.exception!!.message,
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    })
+//                Toast.makeText(
+//                    this@SignUpActivity,
+//                    "Now we can register a new user.",
+//                    Toast.LENGTH_SHORT
+//                ).show()
 
             }else {
-                    showErrorSnackBar("Passwords do not match, please enter the password.")
+                showErrorSnackBar("Please fill all the form.")
         }
         }else {
-            showErrorSnackBar("Please fill all the form.")
+            showErrorSnackBar("Passwords do not match, please enter the password.")
+
         }
     }
 
