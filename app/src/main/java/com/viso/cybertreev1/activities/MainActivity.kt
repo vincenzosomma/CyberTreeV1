a66da4a5014d0fc5fe007a78c97bae1df5d18a58
vincenzosomma <77669617+vincenzosomma@users.noreply.github.com>
2021-06-16 09:30:25 +0100

    Main Menu screen

--- app/src/main/java/com/viso/cybertreev1/activities/MainActivity.kt
+++ app/src/main/java/com/viso/cybertreev1/activities/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.viso.cybertreev1.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.viso.cybertreev1.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
     }
+
 }
